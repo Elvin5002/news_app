d48ad3b72964d6c73c58a19d2e9fb2e65e54b596
Elvin <114992009+Elvin5002@users.noreply.github.com>
2024-12-23 13:49:21 +0400

    - add app screenshots - refactor gitignore

--- lib/domain/usecases/get_categories_usecase.dart
+++ lib/domain/usecases/get_categories_usecase.dart
@@ -3,7 +3,7 @@ import 'package:news_app/core/usecases/usecase.dart';
 import 'package:news_app/domain/repository/article_repository.dart';
 import 'package:news_app/service_locator.dart';
 
-class GetCategoriesUsecase implements UseCase<Either, String> {
+class GetCategoriesUsecase extends UseCase<Either, String> {
   @override
   Future<Either> call({String? params}) {
     return sl<ArticleRepository>().getCategories(params!);
